7d85ff1970e6ce73e912db3106ba4dfd101429b3
Μαρκος01 <marksand2001@gmail.com>
2024-01-08 21:20:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", main);
 function main()
 {
     let checkbox = document.getElementById('veure');
-    let inputs = document.getElementsByClassName("form-control");
+    let form_inputs = document.getElementsByClassName("form-control");
     //Reiniciem l'estat dels inputs i checkbox en cas d'actualitzar pàgina
     checkbox.checked = false;
-    Array.from(inputs).forEach((input) => {
+    Array.from(form_inputs).forEach((input) => {
         input.value = "";
         input.disabled = true;
     });
@@ -15,13 +15,13 @@ function main()
         if (checkbox.checked)
         {
             // https://stackoverflow.com/questions/3871547/iterating-over-result-of-getelementsbyclassname-using-array-foreach
-            Array.from(inputs).forEach((input) => {
+            Array.from(form_inputs).forEach((input) => {
                 input.disabled = false;
             });
         }
         else
         {
-            Array.from(inputs).forEach((input) => {
+            Array.from(form_inputs).forEach((input) => {
                 input.value = "";
                 input.disabled = true;
             });
@@ -30,5 +30,7 @@ function main()
 
     let regexp_titular  = /([A-Z]{2,})([ ][A-Z]{2,})?([ ][A-Z]{2,})/;
     let regexp_cvv      = /[0-9]{3}/;
-    let regexp_caduca   = /[]/;
+    let regexp_caduca   = /(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])/;
+
+
 }

21a6da52ab6e1049ec52a153e05a26492204db85
Μαρκος01 <marksand2001@gmail.com>
2024-01-10 13:47:12 +0100

    Update js
    Added 3 of 4 regular expressions and implemented them validations

--- index.js
+++ index.js
@@ -28,9 +28,137 @@ function main()
         }
     });
 
-    let regexp_titular  = /([A-Z]{2,})([ ][A-Z]{2,})?([ ][A-Z]{2,})/;
-    let regexp_cvv      = /[0-9]{3}/;
-    let regexp_caduca   = /(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])/;
+    let titular     = document.getElementById("owner");
+    let cvv         = document.getElementById("cvv");
+    let tarja       = document.getElementById("cardNumber");
+    let caducitat   = document.getElementById("expireDate");
+
+    console.log(inputs);
+
+    titular.addEventListener("input", function()
+    {
+        nomesLletres(this);
+    });
+
+    cvv.addEventListener("input", function()
+    {
+        nomesNums(this);
+    });
+
+    tarja.addEventListener("input", function()
+    {
+        nomesNums(this);
+    });
+
+    caducitat.addEventListener("input", function()
+    {
+        nomesNums(this);
+    });
+
+    let arrayValidacions = []; // Array on guardarem el resultat de les validacions
+
+    titular.addEventListener("focusout", function()
+    {
+        // https://www.shecodes.io/athena/5558-find-index-of-elements-in-array-with-matching-values-javascript
+        let titularIndex = arrayValidacions.findIndex(x => x.camp == "titular");
+
+        if (titularIndex != -1)
+        {
+            arrayValidacions[titularIndex].valid = validarTitular(this);
+        }
+        else
+        {
+            arrayValidacions.push({ camp: "titular", valid: validarTitular(this) });
+        }
+    });
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
+    cvv.addEventListener("input", escripturaCVV);
+
+    cvv.addEventListener("focusout", function()
+    {
+        let cvvIndex = arrayValidacions.findIndex(x => x.camp == "cvv");
+
+        if (cvvIndex != -1)
+        {
+            arrayValidacions[cvvIndex].valid = validarCVV(this);
+        }
+        else
+        {
+            arrayValidacions.push({ camp: "cvv", valid: validarCVV(this) });
+        }
+    });
+
+    caducitat.addEventListener("input", escripturaCaducitat);
+
+    caducitat.addEventListener("focusout", function()
+    {
+        let caduIndex = arrayValidacions.findIndex(x => x.camp == "caducitat");
+
+        if (caduIndex != -1)
+        {
+            arrayValidacions[caduIndex].valid = validarCaducitat(this);
+        }
+        else
+        {
+            arrayValidacions.push({ camp: "caducitat", valid: validarCaducitat(this) });
+        }
+
+        console.log(arrayValidacions);
+    });
+}
+
+function nomesLletres(input)
+{
+    input.value = input.value.replace(/[^a-zA-Z\s]/, '').toUpperCase();
+}
+
+function nomesNums(input)
+{
+    input.value = input.value.replace(/[^0-9]/, '');
+}
+
+function validarTitular(input)
+{
+    let regexp_titular = /^([A-Z]{2,})([ ][A-Z]{2,})?([ ][A-Z]{2,})$/;
+
+    return regexp_titular.test(input.value);
+}
+
+function escripturaCVV(e)
+{
+    let cvv = e.target.value;
+
+    e.target.value = cvv.replace(/\D/, '').slice(0, 3);
+}
+
+function validarCVV(input)
+{
+    let regexp_cvv = /^[0-9]{3}$/;
+
+    return regexp_cvv.test(input.value);
+}
+
+function escripturaCaducitat(e)
+{
+    let caducitat = e.target.value;
+
+    let inputFinal = caducitat.replace(/\D/, '');
+
+    // Afegim la barra
+    if (inputFinal.length >= 2)
+    {
+        inputFinal = inputFinal.slice(0, 2) + "/" + inputFinal.slice(2);
+    }
+
+    // No més llarg que els numeros de mes i any separats per la barra
+    e.target.value = inputFinal.slice(0, 5);
+}
 
 
+function validarCaducitat(input)
+{
+    let regexp_caduca = /(^0[1-9]|1[0-2])\/(\d{2}$)/;
+
+    return regexp_caduca.test(input.value);
 }
